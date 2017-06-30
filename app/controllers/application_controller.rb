609f5ff899ec1c77976bd97e1510dbd094fe40bb
trandinhtan3360 <trandinhtan.web@gmail.com>
2017-06-30 16:51:39 +0700

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def correct_user
+    @user = User.find_by params[:id]
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".please_login."
+      redirect_to login_url
+    end
+  end
+
+  def verify_admin
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,21 @@ class User < ApplicationRecord
   has_many :comment
   has_many :orders 
   has_many :microposts, dependent: :destroy
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :passwords
   before_save :downcase_email
+  attr_accessor :remember_digest
+  attr_accessor :current_user
+  
   before_save{ email.downcase! }
-  validates :name,  presence: true, length:{ maximum: Settings.minimum }
+  validates :name,  presence: true, length:{ maximum: Settings.maximum }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length:{ maximum: Settings.maximum },
    format:{ with: VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false }
-  validates :email, presence: true, length:{ maximum: Settings.maximum }
+  
   has_secure_password
   validates :password, presence: true, length:{ minimum: Settings.minimum }, allow_nil: true
 
+  scope :newest, ->{order created_at: :desc}
   class << self
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -22,13 +26,13 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
-  end
 
-  def remember
-    remember_token = new_token
-    update_attribute(:remember_digest, digest(remember_token))
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
   end
-
+    
   def authenticated?(attribute, token)
     digest = self.send("#{attribute}_digest")
     return false if digest.nil?
