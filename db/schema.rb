2573ce31e7db9c369f812575684dd10dba78ea7d
trandinhtan3360 <trandinhtan.web@gmail.com>
2017-06-30 16:51:39 +0700

    login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704052627) do
+
+ActiveRecord::Schema.define(version: 20170626035327) do
 
   create_table "categori_products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "categori_id"
@@ -102,9 +103,9 @@ ActiveRecord::Schema.define(version: 20170704052627) do
     t.string "address"
     t.string "role"
     t.string "password_digest"
-    t.string "remember_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "reset_digest"
+    t.string "password"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "categori_products", "categoris"
