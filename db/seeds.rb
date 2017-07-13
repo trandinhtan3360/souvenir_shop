b5d0f02199282867a403469278da645ba606a865
trandinhtan3360 <trandinhtan.web@gmail.com>
2017-07-13 12:29:56 +0700

    edit commit 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+  User.create! name: "trandinhtan", email: "trandinhtan{n}@gmmail.com", password: "12345678"
+end
+
+User.create!(name:  "Tran Dinh Tan ",
+             email: "admin@gmail.com",
+             password: "123456789",
+             password_confirmation: "123456789",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
+end
